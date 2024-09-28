420d231680fdddd5c33dc91455c487ac657cda5c
plutok <plutoka9@gmail.com>
2024-09-28 14:12:23 +0200

    Update nyc_tlc_yellow_trip_calculations.py

--- nyc_tlc_yellow_trip_calculations.py
+++ nyc_tlc_yellow_trip_calculations.py
@@ -1,14 +1,34 @@
-# Questions
-# 1. Calculate the shortest and longest (tripDistance) trips by time of day
+import argparse
 import csv
+import logging
+import os
+
 from datetime import datetime, timedelta
 
-# Function to categorize time of day
-# Categories: 
-#   MORNING 06:00 hours to 12:00 hours
-#   AFTERNOON 12:00 hours to 18:00 hours
-#   EVENING 18:00 hours to 22:00 hours
-#   NIGHT 22:00 hours to 06:00 hours
+# Initialize logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Initialize the argument parser
+parser = argparse.ArgumentParser(description='Calculates using trip data for a specific month.')
+
+# Add an argument for the input file path
+parser.add_argument('input_file_path', type=str, help='Input path of data, which is stored in csv')
+
+# Add an argument for outputh folder path
+parser.add_argument('output_folder_path', type=str, help='Path of folder where the files will be saved')
+
+# Parse the arguments
+args = parser.parse_args()
+
+
+""" 
+Function to categorize time of day
+Categories: 
+    MORNING 06:00 hours to 12:00 hours
+    AFTERNOON 12:00 hours to 18:00 hours
+    EVENING 18:00 hours to 22:00 hours
+    NIGHT 22:00 hours to 06:00 hours
+"""
 
 def get_time_of_day(hour):
     if 6 <= hour < 12:
@@ -47,23 +67,44 @@ def is_valid_trip(trip_distance, pickup_time, dropoff_time):
     return True  # Valid trip data
 
 
-
-# Specify the path for the output CSV file
-input_file = r'C:\_work\zeiss\yellow_tripdata_2024-01.csv'
-output_file = r'C:\_work\zeiss\yellow_tripdata_summary.csv'
-
-
-# Initialize dictionaries to store shortest and longest trips
-shortest_trips = {}
-longest_trips = {}
+def read_csv(file_path):
+    with open(file_path, mode='r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
 
 
-# Read the CSV file
-with open(input_file, mode='r') as file:
-    csv_reader = csv.DictReader(file)
+# Function to write data to a CSV file with exception handling
+def write_csv(data, output_folder_path, file_name):
+    # Ensure the folder exists
+    os.makedirs(output_folder_path, exist_ok=True)  # Creates the folder if it does not exist
     
-    # Iterate over the CSV rows with enumerate, starting at 1, usefull for testing
-    for row_num, row in enumerate(csv_reader, start=1):
+    # Combine folder and file name
+    output_file_path = os.path.join(output_folder_path, file_name)  
+    try:
+        with open(output_file_path, mode='w', newline='') as file:
+            fieldnames = data[0].keys()
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(data)
+        logging.info(f'The results have been saved to {output_file_path}')
+    except Exception as e:
+        logging.error(f'An error occurred while writing to the file: {e}')
+
+#Calculate the amounts paid (tipAmount, tollsAmount, totalAmount) by airports (rateCodeId)
+def calculate_trip_distance(csv_data):
+    """
+    Task: Calculate the shortest and longest (tripDistance) trips by time of day
+    """
+    
+    # Initialize dictionaries to store shortest and longest trips
+    shortest_trips = {}
+    longest_trips = {}
+
+    # Iterate over the CSV rows with enumerate, starting at 1, useful for testing
+    for row_num, row in enumerate(csv_data, start=1):
+        try:
             trip_distance = float(row['trip_distance'])
             pickup_time = datetime.strptime(row['tpep_pickup_datetime'], '%Y-%m-%d %H:%M:%S')
             dropoff_time = datetime.strptime(row['tpep_dropoff_datetime'], '%Y-%m-%d %H:%M:%S')
@@ -87,18 +128,34 @@ with open(input_file, mode='r') as file:
             # Update longest trip for the specific time of day
             if trip_time_of_day not in longest_trips[trip_date] or trip_distance > longest_trips[trip_date][trip_time_of_day]:
                 longest_trips[trip_date][trip_time_of_day] = trip_distance 
-                #print(trip_date, trip_time_of_day, row_num, pickup_time, trip_distance)
-            
-            
-# Write the results to a new CSV file
-with open(output_file, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['trip_date', 'trip_time_of_day', 'shortest_trip_distance', 'longest_trip_distance'])
-    
+                
+        except ValueError as e:
+            logging.error(f'Row {row_num}: Invalid value - {e}')
+
+    # Transform the data into a list of dictionaries
+    data = []
     for trip_date in shortest_trips:
         for trip_time_of_day in shortest_trips[trip_date]:
-            writer.writerow([trip_date, trip_time_of_day, shortest_trips[trip_date][trip_time_of_day], longest_trips[trip_date][trip_time_of_day]])
+            data.append({
+                'date': trip_date,
+                'time_of_day': trip_time_of_day,
+                'shortest_distance': shortest_trips[trip_date][trip_time_of_day],
+                'longest_distance': longest_trips[trip_date][trip_time_of_day]
+            })
+            
+    return data
 
-print("The results have been saved to trip_summary.csv")       
+
+trip_distance_entity = 'trip_distance_summary.csv'
+
+
+# Read the CSV file
+csv_data = read_csv(args.input_file_path)
+
+# Calculate shortest and longest trip by time of day
+trip_distance_data = calculate_trip_distance(csv_data)
+
+# Write the results to a new CSV file
+write_csv(trip_distance_data, args.output_folder_path, trip_distance_entity)
 
         
